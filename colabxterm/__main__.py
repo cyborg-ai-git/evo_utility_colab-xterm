e59c11a740eb9125c8749a1332f827c085dee3fe
CyborgAI <cyborgai.mail@gmail.com>
2025-09-20 16:48:34 +0200

    fixed: command args

--- colabxterm/__main__.py
+++ colabxterm/__main__.py
@@ -6,9 +6,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog='python -m colabxterm')
     parser.add_argument("-p", "--port", type=int,
                         help="port number", default=10000)
-    parser.add_argument("command", help="Commands to run", nargs='*')
-    args = parser.parse_args()
+    parser.add_argument("command", help="Commands to run", nargs=argparse.REMAINDER)
+    args, unknown = parser.parse_known_args()
+    # Combine parsed command args with any unknown args
+    command = args.command + unknown
     port = args.port
-    command = args.command
     term = colabxterm.XTerm(command, port)
     term.open()
